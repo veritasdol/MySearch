88e607ca1bd86502f19c75cc68f815df1ca81767
veritasdol <veritas.dol@gmail.com>
2023-01-04 09:08:35 +0100

    add apisearch

--- content/js/script.js
+++ content/js/script.js
@@ -5,12 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const phrase = document.querySelector('#text').value.replace(/ /ig, '+');
         const myHeaders = new Headers();
-        const myRequest = new Request(`https://www.google.com/search?q=${phrase}`, {
+        const apikey = 'AIzaSyARQNOjkqYiKVR3D1HgOLXMX03sjc7kp4o'
+        const cx = 'e76915b462b7741f3'
+        const myRequest = new Request(`https://www.googleapis.com/customsearch/v1?key=${apikey}&cx=${cx}&q=${phrase}`, {
             method: 'GET',
             headers: myHeaders,
-            mode: 'no-cors',
+            // mode: 'no-cors',
             cache: 'default',
-            query: 'codeforgeek'
           });
 
         fetch(myRequest)
